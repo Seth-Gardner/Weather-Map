8fd0d51f8fd65e70f0233484ef1694305c5134c3
Seth Gardner <seth.gardner.a@gmail.com>
2021-09-27 17:20:25 -0500

    adding files

---
+++ js/keys.js
@@ -0,0 +1,5 @@
+// I kept these in a gitignore for security reasons but I'm keeping them available for showcasing reasons
+
+const mapboxAPIKey = `pk.eyJ1Ijoic2V0aC1nYXJkbmVyIiwiYSI6ImNrcjUyNjdpNjMxbXYycG5scG1pNDVmaTYifQ.yxOnF4_xbCUX80QkApTqAw`
+
+const openweatherAPIKey = `5effeea2345f73c219cb62f6110cf94e`
